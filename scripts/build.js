680a84077953749e1561799246fbfa645368483f
Christophe Massolin <christophe.massolin@gmail.com>
2022-10-12 23:28:00 +0200

    upgrade package and enhance commonjs support

--- scripts/build.js
+++ scripts/build.js
@@ -1,5 +1,6 @@
 import { Extractor, ExtractorConfig } from "@microsoft/api-extractor";
 import { spawnSync } from "child_process";
+import { writeFileSync } from "fs";
 import { resolve } from "path";
 import { ROOT_DIR, SPAWN_OPTIONS } from "./common.js";
 
@@ -7,8 +8,18 @@ async function main() {
   await import("./dev.js");
 
   const build = "npx esbuild --bundle --target=es6 lib/index.ts";
-  spawnSync(`${build} --format=cjs --outfile=dist/index.cjs.js`, SPAWN_OPTIONS);
-  spawnSync(`${build} --format=esm --outfile=dist/index.esm.js`, SPAWN_OPTIONS);
+  spawnSync(`${build} --format=cjs --outfile=dist/cjs/index.js`, SPAWN_OPTIONS);
+  spawnSync(`${build} --format=esm --outfile=dist/esm/index.js`, SPAWN_OPTIONS);
+
+  writeFileSync(
+    "./dist/cjs/package.json",
+    JSON.stringify({ type: "commonjs" }, null, 2)
+  );
+
+  writeFileSync(
+    "./dist/esm/package.json",
+    JSON.stringify({ type: "module" }, null, 2)
+  );
 
   const extractorConfigPath = resolve(ROOT_DIR, `api-extractor.json`);
   const extractorConfig =
